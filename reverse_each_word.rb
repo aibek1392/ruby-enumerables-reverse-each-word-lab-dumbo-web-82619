5b1b8c3fcd505cde59219e19b814fea739b65c21
Aibek Ozhorov <ozhorov@gmail.com>
2019-08-15 23:28:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,15 @@
 def reverse_each_word(str)
 sqr = str.split
+sqr.each do |word|
+  word.reverse!
+end
+sqr.join(" ")
+end
+reverse_each_word("Hello how are you")
+
+  
+  def reverse_each_word(str)
+sqr = str.split
 sqr.collect do |word|
   word.reverse!
 end
@@ -7,10 +17,6 @@ sqr.join(" ")
 end
 reverse_each_word("Hello how are you")
 
- sqr.collect {|word| word.reverse!}
-
-  
-  
   
   
   
